497add90a42c2027f1a15893190e1b9788beaf73
Valex4 <yam778123@gmail.com>
2023-12-15 09:54:49 -0600

    [UPDATE] Form Experiencia lab and GeneralData

--- src/components/atoms/Input.jsx
+++ src/components/atoms/Input.jsx
@@ -4,7 +4,7 @@ function Input({ type, name, dato, onchange, activo}) {
     return ( 
         <>
             <div className="mt-2">
-                <Field type={type} disabled={activo} name={name} value={dato} onChange={onchange} className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset outline-none focus:ring-[#18386B] sm:text-sm sm:leading-6" />          
+                <Field type={type} disabled={activo} name={name} value={dato} onChange={onchange} className="block w-full px-2 pl-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset outline-none focus:ring-[#18386B] sm:text-sm sm:leading-6" />          
             </div>
         </>
      );
