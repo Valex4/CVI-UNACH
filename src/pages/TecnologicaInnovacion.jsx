cefdb7398f3c94fa8b407b00ac6d5cf2b920bd1c
Valex4 <yam778123@gmail.com>
2023-12-18 13:12:46 -0600

    [ADD] FormInnovacion, FormIdiomas, FormDocumentosTrabajo and refactor styles

--- src/pages/TecnologicaInnovacion.jsx
+++ src/pages/TecnologicaInnovacion.jsx
@@ -4,21 +4,91 @@ import { useState } from "react";
 import FormDesarrolloSoftware from '../components/organism/FormDesarrolloSoftware'
 import FormDesarrollosT from "../components/organism/FormDesarrollosT";
 import FormInnovacion from "../components/organism/FormInnovacion"
+import Title from "../components/atoms/Title";
 
 
 function TecnologicaInnovacion() {
     const [opcionSeleccionada, setOpcionSeleccionada] = useState('');
+
+    const renderTableInnovacion = ( ) => {
+        return(
+            <div className=" mx-auto mt-8 pl-8 pr-8 p-5 ">
+                              <table className="min-w-full bg-white border-gray-300 drop-shadow-2xl  rounded-3xl ">
+                              <thead className=" w-full h-10 ">
+                              <tr className="rounded-lg">
+                                  <th className="p-2"><Title level="h3" text="Innovaciones" /> </th>
+                              </tr>
+                              </thead>
+                              <thead>
+                              <tr className=" bg-[#667DA3] text-white">
+                                  <th className="py-2 px-4 border-b text-left">Tipo innovacion</th>
+                                  <th className="py-2 px-4 border-b text-left">Potencial de cobertura</th>
+                                  <th className="py-2 px-4 border-b text-left">Mecanismo de propiedad intelectual</th>
+                                  <th className="py-2 px-4 border-b text-left">Acciones</th>
+                              </tr>
+                              </thead>
+                              <tbody>
+                              <tr>
+                                  <td className="py-2 px-4 border-b">Animatronicos reales</td>
+                                  <td className="py-2 px-4 border-b">UPCHIAPAS</td>
+                                  <td className="py-2 px-4 border-b">11/11/2022</td>
+                                  <td className="py-2 px-4 border-b">
+                                  <button className="bg-[#758AAC] text-black w-9 h-10 rounded-full">
+                                  <span className="material-icons-sharp">
+                                      edit
+                                      </span>
+                                  </button>
+                                  <button className="bg-[#758AAC] text-black w-9 h-10 rounded-full ml-2 hover:bg-red-600 ">
+                                  <span className="material-symbols-outlined">
+                                  delete
+                                  </span>
+                                  </button>
+                                  </td>
+                              </tr>
+                              </tbody>
+                              <tbody>
+                              <tr>
+                                  <td className="py-2 px-4 border-b">Restos espaciales</td>
+                                  <td className="py-2 px-4 border-b">UNACH</td>
+                                  <td className="py-2 px-4 border-b">21/05/2012</td>
+                                  <td className="py-2 px-4 border-b">
+                                  <button className="bg-[#758AAC] text-black w-9 h-10 rounded-full">
+                                  <span className="material-icons-sharp">
+                                      edit
+                                      </span>
+                                  </button>
+                                  <button className="bg-[#758AAC] text-black w-9 h-10 rounded-full ml-2  hover:bg-red-600  ">
+                                  <span className="material-symbols-outlined">
+                                  delete
+                                  </span>
+                                  </button>
+                                  </td>
+                              </tr>
+                              </tbody>
+                          </table>
+                          </div>
+          )
+    }
+
+
+
+
+
     const renderFormulario = () => {
         switch (opcionSeleccionada) {
             case 'op1':
               return(<FormDesarrollosT/>);
             case 'op2':
-                return(<FormInnovacion/>);
+                return(
+                <>
+                    <FormInnovacion/>
+                    {renderTableInnovacion()}
+                </>      
+                );
             case 'op3':
               return(<FormDesarrolloSoftware/>);
             default:
               setOpcionSeleccionada('op1')
-              return(<FormDesarrollosT/>);
           } 
     }  
     return (
